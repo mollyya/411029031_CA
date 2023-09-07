79a8087ff8acb39a59c2f0f9eb8085adaece4b12
mollyya <144211230+mollyya@users.noreply.github.com>
2023-09-07 15:37:54 +0800

    Add files via upload

---
+++ 01 (1).py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=5
+print(f"變數x的值是:{x}")
+print(f"變數x的記憶體地址是:{id(x)}")
+
+
+# In[ ]:
+
+
+
+
---
+++ temp.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x=5
+print(f"變數x的值是:{x}")
+print(f"變數x的記憶體地址是:{id(x)}")
